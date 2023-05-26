9330cdbd9ccff30295d18cfaa83a314eb86da5d2
bea <beatriz.l@lyncas.net>
2023-05-26 13:28:27 -0300

    :sparkles: feat[components]: title
    - Added feature for description down below title
    - Refactor code

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,30 +1,33 @@
-import { Flex, Title as MantineTitle } from "@mantine/core";
+import { Box, Flex, Group, Title as MantineTitle } from "@mantine/core";
 
 type ComponentProps = {
   title: string;
+  description?: string;
   children?: any;
 };
 
-export function Title({ title, children }: ComponentProps) {
+export function Title({ title, description, children }: ComponentProps) {
   return (
     <Flex direction={{ base: 'column', sm: 'row' }}
       gap={{ base: 'sm', sm: 'sm' }}
       justify={{ sm: 'space-between' }}>
-      <MantineTitle
-        style={{
-          color: '#000',
-          fontWeight: 'bold',
-          textDecoration: 'none'
-        }}
-      >
-        {title}
-      </MantineTitle>
 
-      <Flex direction={{ base: 'column', sm: 'row' }}
-        gap={{ base: 'sm', sm: 'sm' }}
-        justify={{ sm: 'space-between' }}>
+      <Box>
+        <MantineTitle
+          style={{
+            color: '#000',
+            fontWeight: 'bold',
+            textDecoration: 'none'
+          }}
+        >
+          {title}
+        </MantineTitle>
+        <small>{description}</small>
+      </Box>
+
+      <Group>
         {children}
-      </Flex>
+      </Group>
     </Flex>
   )
 }
