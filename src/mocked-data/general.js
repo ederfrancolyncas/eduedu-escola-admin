5e554665d76447bbee9ccf69a0b4dfc7dfa979d1
bea <beatriz.l@lyncas.net>
2023-05-26 16:08:51 -0300

    :truck: mocked data

---
+++ src/mocked-data/general.js
@@ -0,0 +1,19 @@
+export const profilesSample = [
+    { value: 'direcao/coordenacao', label: 'Direção/Coordenação' },
+    { value: 'professor', label: 'Professor' }
+]
+
+export const usersSample = [
+    {
+        name: 'Antônio Carlos Ribeiro',
+        email: 'antonio.c@escolaxyz.com.br',
+        cpf: 12345678,
+        profile: 'Direção/Coordenação'
+    },
+    {
+        name: 'Alice Dias',
+        email: 'alice.d@escolaxyz.com.br',
+        cpf: 12345678,
+        profile: 'Direção/Coordenação'
+    },
+];
