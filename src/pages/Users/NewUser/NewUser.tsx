7a1a8e6427a0dee5956ebf2da89326177d828201
bea <beatriz.l@lyncas.net>
2023-05-26 16:16:03 -0300

    :sparkles: feat[new-user]: implemented content

--- src/pages/Users/NewUser/NewUser.tsx
+++ src/pages/Users/NewUser/NewUser.tsx
@@ -1,9 +1,42 @@
+import { Button, Grid, Group, Select, TextInput } from "@mantine/core";
 import { Title } from "../../../components/Title/Title";
 
+// TODO: change mocked data for real data when backend&&DB is ready
+import { profilesSample } from '../../../mocked-data/general'
+
 export function NewUserPage() {
+
     return (
         <>
             <Title title="Novo usuário" />
+
+            <Grid>
+                <Grid.Col span={3}>
+                    <TextInput
+                        label="Nome"
+                        placeholder="Nome"
+                    />
+                </Grid.Col>
+                <Grid.Col span={3}>
+                    <TextInput
+                        label="CPF"
+                        placeholder="CPF"
+                    />
+                </Grid.Col>
+                <Grid.Col span={3}>
+                    <TextInput
+                        label="Email"
+                        placeholder="Email"
+                    />
+                </Grid.Col>
+                <Grid.Col span={3}>
+                    <Select data={profilesSample} label="Perfil" placeholder="Selecione" />
+                </Grid.Col>
+            </Grid>
+            <Group position="right" mt={30}>
+                <Button variant="outline">Cancelar</Button>
+                <Button disabled>Salvar</Button>
+            </Group>
         </>
     )
 }
