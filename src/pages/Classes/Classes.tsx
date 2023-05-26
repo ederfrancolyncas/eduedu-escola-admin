7ab4f55c44741289a74c81fb796394c4317c0d60
bea <beatriz.l@lyncas.net>
2023-05-26 08:39:44 -0300

    :sparkles: feat[page]: new page for Classes

---
+++ src/pages/Classes/Classes.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mantine/core";
+import { Title } from "../../components/Title/Title";
+
+export function ClassesPage() {
+  return (
+    <Title title="Turmas">
+      <Button>Nova turma</Button>
+    </Title>
+  );
+}
