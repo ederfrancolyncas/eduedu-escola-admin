902725956f0eec29575b9c61e4d25cb15b04a580
bea <beatriz.l@lyncas.net>
2023-05-29 14:37:44 -0300

    :sparkles: feat[forgot-pass]: modal + burned useless code

--- src/pages/Auth/Login/Index.tsx
+++ src/pages/Auth/Login/Index.tsx
@@ -1,67 +1,79 @@
-// General:
+// Utils & Aux:
+import { useDisclosure } from '@mantine/hooks';
 import { Link } from "@tanstack/router";
 
-// Form:
+// Components:
 import {
+  Modal,
   Center,
   Box,
   Group,
   Button,
   Image,
   BackgroundImage,
+  TextInput,
 } from "@mantine/core";
-
-// Style:
-import logo from "~/assets/logos/eduedu-branca.svg";
-import bg from "~/assets/backgrounds/login.svg";
 import { RoundedInput } from "../../../components/RoundedInput/RoundedInput";
 import { RoundedButton } from "../../../components/RoundedButton/RoundedButton";
 
+// Assets:
+import logo from "~/assets/logos/eduedu-branca.svg";
+import bg from "~/assets/backgrounds/login.svg";
+
 export const Login = () => {
 
+  const [openedForgotPass, { open, close }] = useDisclosure(false);
+
   return (
-    <BackgroundImage src={bg} h="100vh">
-      <Center maw={400} h={800} mx="auto">
-        <Box sx={{ minWidth: "100%" }}>
-          <Image
-            maw={180}
-            mb={50}
-            mx="auto"
-            radius="md"
-            src={logo}
-            alt="Logo EduEdu Escola"
-          />
-
-          <Group position="center" mt="md" mb={50}>
-            <span style={{ color: "white " }}>Portal Administrativo</span>
-          </Group>
-
-          <form>
-            <RoundedInput
-              label="Email"
-              placeholder="Digite o email cadastrado"
+    <>
+      <BackgroundImage src={bg} h="100vh">
+        <Center maw={400} h={800} mx="auto">
+          <Box sx={{ minWidth: "100%" }}>
+            <Image
+              maw={180}
+              mb={50}
+              mx="auto"
+              radius="md"
+              src={logo}
+              alt="Logo EduEdu Escola"
             />
 
-            <RoundedInput
-              label="Senha"
-              placeholder="Digite sua senha"
-            />
-
-            <Group position="right" mt="md" mb={30}>
-              <Link
-                to="/esqueci-a-senha"
-                style={{ color: "white ", textDecoration: "none" }}
-              >
-                Esqueci a senha
-              </Link>
+            <Group position="center" mt="md" mb={50}>
+              <span style={{ color: "white " }}>Portal Administrativo</span>
             </Group>
 
-            <Group mt="md">
-              <RoundedButton text="Entrar" fullWidth />
-            </Group>
-          </form>
-        </Box>
-      </Center>
-    </BackgroundImage>
+            <form>
+              <RoundedInput
+                label="Email"
+                placeholder="Digite o email cadastrado"
+              />
+
+              <RoundedInput
+                label="Senha"
+                placeholder="Digite sua senha"
+              />
+
+              <Group position="right" mt="md" mb={30}>
+                <Link
+                  onClick={open}
+                  style={{ color: "white ", textDecoration: "none" }}
+                >
+                  Esqueci a senha
+                </Link>
+              </Group>
+
+              <Group mt="md">
+                <RoundedButton text="Entrar" fullWidth />
+              </Group>
+            </form>
+          </Box>
+        </Center>
+      </BackgroundImage>
+
+      <Modal opened={openedForgotPass} onClose={close} title="Esqueci a senha" centered>
+        <TextInput mb={20} label="Email de cadastro" placeholder="Email" />
+        <Button variant="outline" fullWidth>Enviar</Button>
+      </Modal>
+    </>
   )
 }
