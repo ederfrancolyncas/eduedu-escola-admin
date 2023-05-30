af00b751615838d96415723daf77f9936ac65202
Guilherme Tabuada <ggtbh@outlook.com>
2023-05-30 16:05:12 -0300

    feat: query provider setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { RouterProvider } from "@tanstack/router";
 import { router } from "./routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // Style:
 import { ThemeProvider } from "./providers/ThemeProvider";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
--- src/api/user.ts
+++ src/api/user.ts
@@ -1,3 +1,36 @@
+import { useCallback } from "react";
+import { API } from "./base";
+import { useQuery } from "@tanstack/react-query";
+
+export type UserStatus = "ACTIVE" | "INACTIVE";
+export type UserRole = "MASTER" | "ADMIN" | "USER";
+
 export type User = {
-    accessToken: string | null
+  accessToken: string | null;
+  id: string;
+  status: UserStatus;
+  password: string;
+  name: string;
+  email: string;
+  document: string;
+  profile: string;
+  role: UserRole;
+  schoolId: string;
+  created_at: Date;
+  updated_at: Date;
+};
+
+class UserAPI extends API {
+  static async getAll() {
+    const { data } = await this.api.get<User[]>("/users");
+    return data;
+  }
+}
+
+export function useUserGetAll() {
+  const handler = useCallback(function () {
+    return UserAPI.getAll();
+  }, []);
+
+  return useQuery(["USER_ALL"], handler, {});
 }
