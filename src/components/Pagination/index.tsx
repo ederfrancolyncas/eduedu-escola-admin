083268d2f8e981e10c8dcba78d815f7214b95e26
bea <beatriztattooer@gmail.com>
2023-06-19 09:06:16 -0300

    :construction: wip[users]: pagination state

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -6,17 +6,21 @@ import {
     Select,
     Pagination as PaginationMantine
 } from "@mantine/core"
+import { useState } from "react";
 
 type componentProps = {
     totalPages: any
 }
 
 export function Pagination({ totalPages }: componentProps) {
+    const [activePage, setPage] = useState(1);
     return (
         <Group position="apart">
             <div></div>
             <Flex align="center" justify="center">
                 <PaginationMantine
+                    value={activePage}
+                    onChange={setPage}
                     total={totalPages}
                     withControls={false}
                 />
